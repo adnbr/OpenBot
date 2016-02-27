a7c692484f7ba1506cafcc604cc15dac5ab74f0a
adnbr <git@adnbr.co.uk>
2016-02-27 22:34:56 +0000

    Pin changes, hardware, housing added to git
    KiCad schematic & PCB. First revision.
    Housing faceplate files (FPD, SVG and DXF formats)
    Code update to reflect pin changes in hardware

--- code/openbot-ntp-ssl/openbot-ntp-ssl.ino
+++ code/openbot-ntp-ssl/openbot-ntp-ssl.ino
@@ -1,26 +1,27 @@
-/*  
+/*
  *  Leeds Hackspace OpenBot
  *  Tweets (@LHSOpenBot) when the space is open for non-keyholders.
  *  For more information ask Aidan, or check the wiki. Flash onto an ESP-12E/NodeMCU dev board.
- *  
+ *
  *  Set up Wifi/API credentials in keys.h.
  *  Alter tweeted text in strings.h.
- *  
+ *
  *  Thingspeak channel fields:
- *  
+ *
  *  +-------+-------+---------+-----------+
  *  | #1    | #2    | #3      | #4        |
  *  +-------+-------+---------+-----------+
  *  | hours | until | message | localutc  |
  *  +-------+-------+---------+-----------+
- *  
+ *
  *  http://www.leedshackspace.org.uk
  *  CC-BY-SA 2016
- *  
+ *
  */
+#include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h> 
+#include <WiFiClientSecure.h>
 #include <WiFiudp.h>
 #include <TimeLib.h>
 #include <Timezone.h>    //https://github.com/JChristensen/Timezone
@@ -29,15 +30,15 @@
 #include "keys.h"
 #include "strings.h"
 
-#define PIN_LED_RED        D0
-#define PIN_LED_GREEN      D1
-#define PIN_BUTTON         D2
-#define PIN_MOTOR_UP       D3
-#define PIN_MOTOR_DOWN     D4
+#define PIN_LED_RED        D3
+#define PIN_LED_GREEN      D4
+#define PIN_BUTTON         D5
+#define PIN_MOTOR_FWD       D2
+#define PIN_MOTOR_REV     D1
 
 #define LIGHT_RED          1
 #define LIGHT_GREEN        2
-#define LIGHT_OFF          0          
+#define LIGHT_OFF          0
 
 #define EEPROM_ADDRESS     0
 #define EEPROM_OK          100
@@ -47,19 +48,19 @@
 #define STOP               3
 #define KNOB_VARIANCE      6 // +/- ideal knob position.
 
-#define NTP_UPDATE_FREQ    21700 // 360 minutes - spreads out NTP load. Onboard oscillator is plenty accurate. 
+#define NTP_UPDATE_FREQ    21700 // 360 minutes - spreads out NTP load. Onboard oscillator is plenty accurate.
 static WiFiClientSecure client;
 WiFiUDP udp;
 
 // Port to listen on for UDP packets
 const unsigned short localUDPPort = 2390;
 
-IPAddress timeServerIP;  
+IPAddress timeServerIP;
 const char* ntpServerName = "uk.pool.ntp.org";     // Pool is preferred
 const char* ntpServerName2 = "time.nist.gov";   // Second choice
 
 const int NTP_PACKET_SIZE = 48;
-byte packetBuffer[NTP_PACKET_SIZE]; 
+byte packetBuffer[NTP_PACKET_SIZE];
 
 //United Kingdom (London, Belfast, Edinburgh)
 TimeChangeRule BST = {"BST", Last, Sun, Mar, 1, 60};        // British Summer Time
@@ -74,87 +75,87 @@ bool turnKnob;
 unsigned long lastKnobCheck;
 
 time_t getNtpTime() {
-  
+
   while (udp.parsePacket() > 0) ; // discard any previously received packets
-  
+
   Serial.println("Sending NTP Request");
   sendNTPpacket(timeServerIP);
-  
+
   uint32_t beginWait = millis();
-  
+
   while (millis() - beginWait < 2000) {
     int size = udp.parsePacket();
     if (size >= NTP_PACKET_SIZE) {
-      
+
       Serial.println("Received NTP Response");
 
       // Read packet into buffer
       udp.read(packetBuffer, NTP_PACKET_SIZE);
       unsigned long secsSince1900;
-      
+
       // Convert four bytes starting at location 40 to a long integer
       secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
       secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
       secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
       secsSince1900 |= (unsigned long)packetBuffer[43];
 
-      // Calculate epoch and calculate the local time from this. 
+      // Calculate epoch and calculate the local time from this.
       time_t utc = secsSince1900 - 2208988800UL;
       localTime = UK.toLocal(utc);
-          
+
       return utc;
     }
   }
 
   Serial.println("No response from NTP server (" + String(timeServerIP) + ")");
   return 0;
-  
+
 }
 
 // send an NTP request to the time server at the given address
 void sendNTPpacket(IPAddress &address) {
-  
+
   // Set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  
+
   // Initialize values needed to form NTP request
   // (see URL above for details on the packets)
   packetBuffer[0] = 0b11100011;   // LI, Version, Mode
   packetBuffer[1] = 0;            // Stratum, or type of clock
   packetBuffer[2] = 6;            // Polling Interval
   packetBuffer[3] = 0xEC;         // Peer Clock Precision
-  
+
   // 8 bytes of zero for Root Delay & Root Dispersion.
   packetBuffer[12]  = 49;
   packetBuffer[13]  = 0x4E;
   packetBuffer[14]  = 49;
   packetBuffer[15]  = 52;
-  
+
   // All NTP fields have been given values, now
-  // send a packet requesting a timestamp.                 
+  // send a packet requesting a timestamp.
   udp.beginPacket(address, 123); //NTP requests are to port 123
   udp.write(packetBuffer, NTP_PACKET_SIZE);
   udp.endPacket();
-  
+
 }
 
 void eepromWriteCheckValue(int value) {
-  
+
   byte lowByte = ((value >> 0) & 0xFF);
   EEPROM.write(EEPROM_ADDRESS, lowByte);
   EEPROM.commit();
-  
+
 }
 
 unsigned int eepromReadCheckValue() {
-  
+
   byte lowByte = EEPROM.read(EEPROM_ADDRESS);
   return lowByte;
-  
+
 }
 
 String displayTime(short hour, short minute) {
-  
+
   String output;
   if (hour < 10) {
     output = "0";
@@ -165,11 +166,11 @@ String displayTime(short hour, short minute) {
   }
   output = output + minute;
   return output;
-  
+
 }
 
 void switchLEDS(char lightMode) {
-  
+
   char red, green;
   switch (lightMode) {
     case LIGHT_OFF:
@@ -179,7 +180,7 @@ void switchLEDS(char lightMode) {
     case LIGHT_RED:
       red = HIGH;
       green = LOW;
-      break;    
+      break;
     case LIGHT_GREEN:
       red = LOW;
       green = HIGH;
@@ -187,16 +188,16 @@ void switchLEDS(char lightMode) {
   }
   digitalWrite(PIN_LED_RED, red);
   digitalWrite(PIN_LED_GREEN, green);
-  
+
 }
 
 
 String buildRequest(int hours, String closingTime, String message, bool tweet = false) {
-  
-  // Construct the actual data to be transmitted - includes the dial number, 
+
+  // Construct the actual data to be transmitted - includes the dial number,
   // expected closing time and the message itself.
-  String data = String("field1=") + hours + 
-                       "&field2=" + closingTime + 
+  String data = String("field1=") + hours +
+                       "&field2=" + closingTime +
                        "&field3=" + message +
                        "&field4=" + localTime; //Second since Jan 1st 1970
 
@@ -207,23 +208,23 @@ String buildRequest(int hours, String closingTime, String message, bool tweet = 
 
   // Construct the HTTP header to be send, and append the data.
   return String("POST ") + api_uri + " HTTP/1.1\r\n" +
-               "Host: " + api_host + "\r\n" + 
+               "Host: " + api_host + "\r\n" +
                "THINGSPEAKAPIKEY: " + api_key + "\r\n" +
                "Content-Type: application/x-www-form-urlencoded\r\n" +
                "Content-Length:"  + data.length() + "\n\n" + data;
-               
+
 }
 
 bool sendHTTPRequest(String httpRequest, int hours) {
-  
+
     // Write the hours value to the eeprom to allow for recovery should the SSL handshake
     // crash the device (sometimes this happens thanks to low/fragmented memory.)
     eepromWriteCheckValue(hours);
     Serial.println("Writing check value to EEPROM");
-    
+
     // Try and connect to the API host. Restart at 5x failures.
     int failCount = 0;
-    
+
     while (!client.connect(api_host, api_port)) {
       failCount++;
       Serial.println("Connection to '"+ String(api_host) + ":" + String(api_port) + "' failed on attempt " + String(failCount) + ". Waiting 1 second to retry.");
@@ -240,9 +241,9 @@ bool sendHTTPRequest(String httpRequest, int hours) {
       return false;
     }
 
-    // Send the HTTP Request to the server. 
+    // Send the HTTP Request to the server.
     client.print(httpRequest);
-    
+
     Serial.println("Sending HTTP Request:");
     Serial.println(httpRequest);
     Serial.print("\n\n");
@@ -256,7 +257,7 @@ bool sendHTTPRequest(String httpRequest, int hours) {
         return false;
       }
     }
-    
+
     Serial.println("Received from server:");
     while(client.available()){
       Serial.print(client.readStringUntil('\r'));
@@ -271,9 +272,9 @@ bool sendHTTPRequest(String httpRequest, int hours) {
     // more we can do on our end.
     eepromWriteCheckValue(EEPROM_OK);
     Serial.println("Clearing check value from EEPROM.");
-    
+
     return true;
-    
+
 }
 
 String createHTTPRequest(int hours) {
@@ -282,16 +283,16 @@ String createHTTPRequest(int hours) {
   short randomNumber;
 
   updateLocalTime();
-  
+
 
     // Generate the HTTP request to be sent to ThingSpeak.
     if (hours == 0) {
-      
+
       // Closed has been selected on the dial. Tweet a "random" closed message.
       randomNumber = random (1, numberClosedStrings);
-      
+
       turnKnob = false;
-      
+
       // Check if the time is correct.
       if (timeStatus() != timeSet) {
         // Add an appropriate failure hashtag.
@@ -302,11 +303,11 @@ String createHTTPRequest(int hours) {
       }
 
       return buildRequest(hours, displayTime(hour(localTime), minute(localTime)), message, true);
-      
+
     } else {
-      
+
       turnKnob = true;
-      
+
       // Space will be open for a period of time, tweet a "random" open message.
       randomNumber = random (1, numberOpenStrings);
 
@@ -316,26 +317,26 @@ String createHTTPRequest(int hours) {
         message = openSpace[randomNumber].leader + hours + " hours" + openSpace[randomNumber].punctuation + " #ntpfailure";
         return buildRequest(hours, "#ntpfailure", message, true);
       }
-      
+
       // Increment the timer, if the new value is over 24 hours (midnight) then subtract 24 to make
       // it into a valid time. If equal to 24 then still subtract 24 so midnight is represented as 00:00
       closingHour = hour(localTime) + hours;
       if (closingHour >= 24) {
         closingHour -= 24;
       }
-      
-  
+
+
       // Round the minutes down to the closest quarter-hour. No need for minute level precision.
       closingMinute = minute(localTime) - (minute(localTime) % 15);
-  
-      // If the hour is singular "1" then we need to use the singular text. Otherwise, add the 
+
+      // If the hour is singular "1" then we need to use the singular text. Otherwise, add the
       // number and append "hours".
       if (hours == 1) {
         message = openSpace[randomNumber].leader + openSpace[randomNumber].singular + openSpace[randomNumber].punctuation + " (Until ~" + displayTime(closingHour, closingMinute) + ")";
       } else {
         message = openSpace[randomNumber].leader + hours + " hours" + openSpace[randomNumber].punctuation + " (Until ~" + displayTime(closingHour, closingMinute) + ")";
       }
-  
+
       return buildRequest(hours, displayTime(closingHour, closingMinute), message, true);
     }
 
@@ -357,7 +358,7 @@ void setup() {
   // Start the serial. start of message line break to clear away
   // from the garbage the ESP8266 dumps out the serial port at boot.
   Serial.begin(115200);
-  Serial.println("\n\nStarting OpenBot..."); 
+  Serial.println("\n\nStarting OpenBot...");
 
   // We are starting to do work now, red LED on.
   switchLEDS(LIGHT_RED);
@@ -372,34 +373,34 @@ void setup() {
     delay(250);
     Serial.print(".");
   }
-  Serial.print("\nConnected to '" + String(ssid) + "' with IP: ");  
+  Serial.print("\nConnected to '" + String(ssid) + "' with IP: ");
   Serial.println(WiFi.localIP());
 
   // Start UDP
   udp.begin(localUDPPort);
-  
+
   // Look up the IP address of the NTP time server, then set up the
   // recurring time polling.
-  WiFi.hostByName(ntpServerName, timeServerIP); 
-  
+  WiFi.hostByName(ntpServerName, timeServerIP);
+
   Serial.println("Syncing with NTP server (" + String(ntpServerName) + ")");
-  
+
   setSyncProvider(getNtpTime);
   setSyncInterval(NTP_UPDATE_FREQ);
-  
+
   // Wait for the time to be synced with NTP.
-  if (timeStatus() == timeNotSet) {  
+  if (timeStatus() == timeNotSet) {
     Serial.println("Could not get time from primary NTP. Attempting secondary NTP server.");
     String httpRequest = buildRequest(0, "ERR:NTP1", "Failed to retrieve time from primary NTP on boot - " + String(timeServerIP), false);
     sendHTTPRequest(httpRequest, 0);
-    WiFi.hostByName(ntpServerName2, timeServerIP); 
+    WiFi.hostByName(ntpServerName2, timeServerIP);
     time_t set = getNtpTime();
     if (set != 0) {
       // Second time sync was successful, set the onboard time.
       setTime(set);
     } else {
       // Thingspeak wants a 15 second delay or it throttles writes and discards data.
-      delay(15500); 
+      delay(15500);
       httpRequest = buildRequest(0, "ERR:NTP2", "Failed to retrieve time from secondary NTP on boot - " + String(timeServerIP), false);
       sendHTTPRequest(httpRequest, 0);
     }
@@ -414,20 +415,20 @@ void setup() {
   if (checkValue != EEPROM_OK) {
     // The device has rebooted before completing the submission.
     Serial.println("Uncompleted submission detected. Resending value '" + String(checkValue) + "'.");
-    
+
     randomSeed(hour() * second() + minute() / analogRead(A0));
-    
+
     String httpRequest = createHTTPRequest(checkValue);
     sendHTTPRequest(httpRequest, checkValue);
   }
-  
+
   // Work complete, green LED lit.
   switchLEDS(LIGHT_GREEN);
-  
+
 }
 
 bool moveKnob() {
-  
+
   int currentPos = analogRead(A0);
 
   // The knob can be divided into 8 quite nicely - 128 - and we want to move to the middle of the
@@ -439,7 +440,7 @@ bool moveKnob() {
 
   // Disabled for the moment, until the H-bridge is connected.
   /*while (!abs(currentPos - targetPos) <= KNOB_VARIANCE) {
-   
+
     // Not there yet. Move that knob!
     char motorDirection = UP;
     if (currentPos > targetPos) {
@@ -449,11 +450,11 @@ bool moveKnob() {
     delay(25);
     controlMotor(STOP);
   } */
-  
+
 }
 
 void controlMotor(char direction) {
-  
+
   char up, down;
   switch (direction) {
     case UP:
@@ -463,24 +464,24 @@ void controlMotor(char direction) {
     case DOWN:
       up = LOW;
       down = HIGH;
-      break;    
+      break;
     case STOP:
       up = LOW;
       down = LOW;
       break;
   }
-  digitalWrite(PIN_MOTOR_UP, up);
-  digitalWrite(PIN_MOTOR_DOWN, down);
-  
+  digitalWrite(PIN_MOTOR_FWD, up);
+  digitalWrite(PIN_MOTOR_REV, down);
+
 }
 
 
 void updateLocalTime() {
-  
+
   Serial.println("Refreshing local time.");
   localTime = UK.toLocal(now());
   Serial.println("Local time is: " + displayTime(hour(localTime), minute(localTime)));
-  
+
 }
 
 void loop() {
@@ -499,7 +500,7 @@ void loop() {
 
     // Seed the RNG. It's still rubbish, but perhaps this will make it a bit better.
     randomSeed(hour() * second() + minute() + knobValue);
-        
+
     String httpRequest = createHTTPRequest(hours);
     sendHTTPRequest(httpRequest, hours);
 
--- readme.md
+++ readme.md
@@ -2,4 +2,21 @@
 
 For more information: https://wiki.leedshackspace.org.uk/wiki/Projects/OpenBot
 
-Rename "keys_example.h" to "keys.h" and fill in the appropriate fields before compiling
+Runs Arduino code on a NodeMCU ESP-12 dev board.
+
+## Software
+
+Rename "keys_example.h" to "keys.h" and fill in the appropriate fields before compiling.
+
+## Hardware
+
+**Caution** - PCB layout/schematic is currently untested.
+
+Schematic and PCB created in KiCad.
+
+Gerbers as submitted to OSHPark are available in the gerbers folder (just upload the zip.) 
+If you don't want to make any changes you can order [directly from them](https://oshpark.com/shared_projects/XAhVpFpk).
+
+## Housing
+
+The front panel was machined by [Schaeffer AG](http://www.schaeffer-ag.de/) and designed in their Front Panel Designer software. The design is also available in SVG and DXF formats (exported from FPD) both with and without reference points in the hope that they might be useful. They are handy in that the text output is in the form of a single, clean path.
